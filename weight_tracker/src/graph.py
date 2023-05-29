3ab64b35f1819f066c89805df9761099a014edd1
veledara <71222580+veledara@users.noreply.github.com>
2023-05-30 00:02:00 +0300

    Now the graph is being built correctly.

--- weight_tracker/src/graph.py
+++ weight_tracker/src/graph.py
@@ -1,27 +1,53 @@
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import numpy as np
+from typing import List
+from datetime import date, datetime
 
-# Generate sample data
-days = np.arange(1, 11)  # Example: 10 days
-weight = [
-    70,
-    69.5,
-    69,
-    68.8,
-    68.2,
-    67.7,
-    68,
-    68.5,
-    68.2,
-    68.7,
-]  # Example: Weight data for each day
 
-# Create a Matplotlib figure
-fig = Figure(figsize=(6, 4), dpi=100, facecolor="lightgray")
-ax = fig.add_subplot(111)
-ax.plot(days, weight, color="red", marker="o")
-ax.set_xlabel("Days")
-ax.set_ylabel("Weight")
-ax.set_title("Weight Statistics")
-ax.grid(True)
+class Mark:
+    date: datetime
+    weight: float
+
+    def __init__(self, date, weight: float) -> None:
+        self.date = date
+        self.weight = float(weight)
+
+    def __repr__(self) -> str:
+        return f"Date: {self.date}, Weight: {self.weight}"
+
+
+class GraphMaker:
+    marks: List[Mark]
+    fig: Figure
+
+    def __init__(self) -> None:
+        self.marks = []
+        self.fig = Figure(figsize=(6, 4), dpi=100, facecolor="lightgray")
+        self.ax = self.fig.add_subplot(111)
+        self.ax.set_xlabel("Days")
+        self.ax.set_ylabel("Weight")
+        self.ax.set_title("Weight Statistics")
+        self.ax.grid(True)
+        self.update_graph()
+
+    def update_graph(self):
+        self.ax.plot(
+            [mark.date for mark in self.marks],
+            [mark.weight for mark in self.marks],
+            color="red",
+            marker="o",
+        )
+        
+
+    def add_mark(self, date: datetime, weight: float) -> None:
+        self.marks.append(Mark(date, weight))
+        self.update_graph()
+
+    def __repr__(self) -> str:
+        return f"Marks are {self.marks}"
+
+
+if __name__ == "__main__":
+    graph = GraphMaker()
+    current_time = datetime.now()
+    graph.add_mark(datetime(2023, 1, 1), 70.6)
+    print(graph)
--- weight_tracker/src/gui.py
+++ weight_tracker/src/gui.py
@@ -1,38 +1,82 @@
+from sys import float_repr_style
 import tkinter as tk
-from weight_tracker.src.graph import fig
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import ttk
+from datetime import datetime
+from graph import GraphMaker
 
 
 class WeightTrackerGUI:
+    graph: GraphMaker
     def __init__(self) -> None:
         self.root = tk.Tk()
         self.root.title("Weight Tracker")
-        self.root.geometry("1024x768")
-        #self.root.resizable(False, False)
+        self.root.geometry("1024x512")
+        # self.root.resizable(False, False)
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
+        self.graph = GraphMaker()
 
         self.main_window_show()
 
     def main_window_show(self):
-        self.weight_change_graph_labelframe = tk.LabelFrame(
-            self.root, bg='lightgray'
-        )
+        self.weight_change_graph_labelframe = tk.LabelFrame(self.root, bg="lightgray")
         self.weight_change_graph_labelframe.grid(
             column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S), padx=20, pady=20
         )
 
-        canvas = FigureCanvasTkAgg(fig, master=self.weight_change_graph_labelframe)
+        self.options_labelframe = tk.LabelFrame(self.root, text="Options")
+
+        self.options_labelframe.grid(
+            column=1, row=0, sticky=(tk.N, tk.W, tk.E, tk.S), padx=20, pady=20
+        )
+
+        self.add_frame = tk.Frame(self.options_labelframe)
+        self.add_frame.grid(
+            column=0, row=1, sticky=(tk.N, tk.W, tk.E, tk.S), padx=20, pady=10
+        )
+        self.date_label = ttk.Label(self.add_frame)
+        self.update_label()
+        self.date_label.grid(
+            column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S), padx=5, pady=5
+        )
+
+        self.weight_entry = ttk.Entry(self.add_frame, width=15)
+        self.weight_entry.grid(
+            column=0, row=1, sticky=(tk.N, tk.W, tk.E, tk.S), padx=5, pady=5
+        )
+
+        self.add_mark_button = ttk.Button(
+            self.add_frame,
+            text="add",
+            command=lambda: self.add_mark_to_graph(self.weight_entry.get().split("|")[0]),
+        )
+        self.add_mark_button.grid(
+            column=0, row=2, sticky=(tk.N, tk.W, tk.E, tk.S), padx=5, pady=5
+        )
+
+    def update_label(self):
+        current_time = datetime.now().strftime(f"%d.%m.%y | %H:%M:%S")
+        self.date_label.config(text=current_time)
+        self.date_label.after(1000, self.update_label)
+
+    def update_graph(self):
+        for widgets in self.weight_change_graph_labelframe.winfo_children():
+             widgets.destroy()
+        self.graph.update_graph()
+        canvas = FigureCanvasTkAgg(
+            self.graph.fig, master=self.weight_change_graph_labelframe
+        )
         canvas.draw()
         canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-        self.options_labelframe = tk.LabelFrame(
-            self.root, text="Options"
-        )
-        self.options_labelframe.grid(
-            column=0, row=1, sticky=(tk.N, tk.W, tk.E, tk.S), padx=20, pady=20
-        )
+    def add_mark_to_graph(self, weight: float):
+        self.weight_validation()
+        self.graph.add_mark(self.date_label.cget("text"), weight)
+        self.update_graph()
+
+    def weight_validation(self):
+        pass
 
     def run(self):
         self.root.mainloop()
